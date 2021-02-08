ac6e97fad542fc964007d3f74b07ffb3f6f569b2
jajugoguma <jajugoguma@gmail.com>
2021-02-08 16:32:37 +0900

    init project

---
+++ src/main/java/com/nhn/rookie8/smaple/account/SmapleAccountApplication.java
@@ -0,0 +1,13 @@
+package com.nhn.rookie8.smaple.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmapleAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmapleAccountApplication.class, args);
+	}
+
+}
